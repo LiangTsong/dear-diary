00fd6fe1a0bc3d203846f91702824dd820b82356
Liang Cong <liangxcong@gmail.com>
2019-11-26 20:34:05 +0800

    Emoji Chart improved. Now the color makes more sense. And modified axis.

--- src/Components/Chart/PolarityEmotionChart.js
+++ src/Components/Chart/PolarityEmotionChart.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {FlexibleXYPlot, AreaSeries, VerticalGridLines, HorizontalGridLines, XAxis, YAxis, Hint, GradientDefs} from 'react-vis';
+import {FlexibleXYPlot, AreaSeries, XAxis, YAxis, Hint, GradientDefs, Crosshair} from 'react-vis';
 
 import './PolarityEmotionChart.css';
 import '../../../node_modules/react-vis/dist/style.css';
@@ -11,44 +11,53 @@ class PolarityEmotionChart extends React.Component {
         this.state = {
             hintValue: null,
             currentIndex: null,
-            emojis: ["🤪🦐🤪", "🌑", "🤪"],
+            crosshairValues: [],
+            emojis: ["🤪", "🤪", "🤪", "🤪", "🤪", "🤪", "🤪"],
+            digests: ["hi", "this", "is", "a", "cool", "diary", "app!"]
         };
     }
 
     render() {
         const DATA = [
-            {x: 1, y: 10},
-            {x: 2, y: 7},
-            {x: 3, y: 15},
+            {x: 0, y: 0.89},
+            {x: 1, y: 0.35},
+            {x: 2, y: 0.11},
+            {x: 3, y: 0.96},
+            {x: 4, y: 0.55},
+            {x: 5, y: 0.68},
+            {x: 6, y: 0.55},
         ];
 
-        const animation = {damping: 10, stiffness: 300};
+        const dates = ["12.1", "12.2", "12.3", "12.4", "12.5", "12.6", "12.7"];
 
         return (
             <div className="polarity-emotion-chart-box">
                 <FlexibleXYPlot
-                    animation={animation}
-                    onMouseLeave={() => this.setState({hintValue: null, currentIndex: null})}>
-                    <VerticalGridLines />
-                    <HorizontalGridLines />
-                    <XAxis />
-                    <YAxis />
+                    colorType="linear"
+                    onMouseLeave={() => this.setState({
+                        hintValue: null, currentIndex: null, crosshairValues: []})}>
+                    <XAxis
+                        tickValues={[0, 1, 2, 3, 4, 5, 6]}
+                        title="日期"
+                        tickFormat={v => `${dates[v]}`}/>
+                    <YAxis hideTicks/>
 
                     <GradientDefs>
                         <linearGradient id="EmotionGradient" x1="0" x2="0" y1="0" y2="1">
-                            <stop offset="0%" stopColor="red" stopOpacity={0.3}/>
-                            <stop offset="20%" stopColor="yellow" stopOpacity={0.3}/>
-                            <stop offset="50%" stopColor="blue" stopOpacity={0.3}/>
-                            <stop offset="100%" stopColor="green" stopOpacity={0.3}/>
+                            <stop offset="0%" stopColor="#FFD60A" stopOpacity={0.4}/>
+                            <stop offset="55%" stopColor="#5AC8FA" stopOpacity={0.3}/>
+                            <stop offset="65%" stopColor="#5AC8FA" stopOpacity={0.3}/>
+                            <stop offset="100%" stopColor="#5856D6" stopOpacity={0.4}/>
                         </linearGradient>
                     </GradientDefs>
 
                     <AreaSeries
                         color={'url(#EmotionGradient)'}
                         onNearestX={(value, {index}) =>
-                            this.setState({hintValue: DATA[index], currentIndex: index})}
+                            this.setState({
+                                hintValue: DATA[index], currentIndex: index, crosshairValues: [DATA[index]]})}
                         data={DATA}
-                        curve="curveNatural"/>
+                        curve="curveCatmullRom"/>
 
                     {this.state.hintValue ?
                     (<Hint value={this.state.hintValue}>
@@ -56,6 +65,13 @@ class PolarityEmotionChart extends React.Component {
                             {this.state.emojis[this.state.currentIndex]}
                         </div>
                     </Hint>) : null}
+
+                    <Crosshair
+                        values={this.state.crosshairValues}
+                        className="invisible-crosshair-tag">
+                        <p>{this.state.digests[this.state.currentIndex]}</p>
+                    </Crosshair>
+
                 </FlexibleXYPlot>
             </div>
         );
